c4d06a02dc810845bcbbff430184f7f77d31eaf2
João Almeida <joaofilipe90@gmail.com>
2021-08-06 19:12:54 +0100

    changes3

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import os
-from main import app 
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Hello World!"
+
 port = int(os.environ.get('PORT', 5000))
 app.run(host='0.0.0.0', port=port)
